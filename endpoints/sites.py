1c8d9e73088f6b02c4ced244f797b4546ffc4b96
timbeccue <timothybeccue@gmail.com>
2019-06-27 17:26:59 -0700

    Fix dynamodb table name conflict

--- endpoints/sites.py
+++ endpoints/sites.py
@@ -61,11 +61,10 @@ def init_from_config(site, config=None):
     table_name = str(site)
     dynamodb.create_table(table_name)
 
-    # Create a dynamodb table to track data from each mount. Does nothing if
+    # Create a dynamodb table to track data from each site. Does nothing if
     # it already exists.
-    for mount in site_mounts:
-        image_dynamodb_name = f"{site}_{mount}_images"
-        dynamodb.create_table(image_dynamodb_name)
+    image_dynamodb_name = f"{site}_images"
+    dynamodb.create_table(image_dynamodb_name)
 
 def get_all_config():
     return dynamodb.scan('site_configurations')
