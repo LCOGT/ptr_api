912570c368e1d59438b1776c942f97db95621798
Valentina Hallefors <vhallefors@lco.global>
2019-07-02 15:35:49 -0700

    update endpoints in data.py

--- ptr_api/endpoints/data.py
+++ ptr_api/endpoints/data.py
@@ -7,9 +7,11 @@ BUCKET_NAME = str(os.environ.get('BUCKET_NAME'))
 def upload(site):
     content = json.loads(request.get_data())
     object_name = f"{site}/{content['object_name']}"
-    return s3.get_presigned_post_url(BUCKET_NAME, object_name)
+    response = s3.get_presigned_post_url(BUCKET_NAME, object_name)
+    return jsonify(response)
 
 def download(site):
     content = json.loads(request.get_data())
     object_name = f"{site}/{content['object_name']}"
-    return s3.get_presigned_url(BUCKET_NAME, object_name)
+    url = s3.get_presigned_url(BUCKET_NAME, object_name)
+    return url
