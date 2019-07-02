98ffb86bbd0f91741d01573140245033051f4efe
Valentina Hallefors <vhallefors@lco.global>
2019-07-02 13:57:50 -0700

    update test_get_presigned_url

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -6,14 +6,16 @@ from botocore.client import Config
 from moto import mock_s3
 
 from flask import Flask
+import re
 
 @mock_s3
 def test_get_presigned_url():
     BUCKET_NAME = 'test_bucket'
     OBJECT_NAME = 'test_object'
     test_url = s3.get_presigned_url
+    verify_is_url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+] [!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url)
 
-    assert test_url is not None
+    assert verify_is_url is not None
 
         
 @mock_s3
