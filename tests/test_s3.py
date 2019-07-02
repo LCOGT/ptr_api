bfc721776b679e5788edda2362760a7573c3355b
Valentina Hallefors <vhallefors@lco.global>
2019-07-02 15:12:48 -0700

    update test_presigned_url

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,10 +1,6 @@
 import pytest
 from ptr_api.aws import s3
-
-import boto3
-from botocore.client import Config
 from moto import mock_s3
-
 from flask import Flask
 import re
 
@@ -13,15 +9,23 @@ def test_get_presigned_url():
     BUCKET_NAME = 'test_bucket'
     OBJECT_NAME = 'test_object'
     url = s3.get_presigned_url(BUCKET_NAME, OBJECT_NAME)
-    verify_is_url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+] [!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url)
 
-    assert verify_is_url is not None
+    verify_url = re.search('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+] [!*\\(\\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url)
+    verify_region = re.search(s3.REGION, url)
+    verify_bucket = re.search(BUCKET_NAME, url)
+    verify_object = re.search(OBJECT_NAME, url)
+
+    assert verify_url is not None
+    assert verify_region is not None
+    assert verify_bucket is not None
+    assert verify_object is not None
 
         
 @mock_s3
 def test_get_presigned_url_error():
     with pytest.raises(Exception):
         assert s3.get_presigned_url(BUCKET_NAME)
+
 
 @mock_s3
 def test_get_presigned_post_url():
@@ -31,14 +35,5 @@ def test_get_presigned_post_url():
        OBJECT_NAME = 'test_object'
 
        json_url = s3.get_presigned_post_url(BUCKET_NAME, OBJECT_NAME)
-       
+
        assert True
-
-
-
-
-
-
-
-
-
