55cc1fc899bc624d278592f536a7ffe73e467b8c
jackie <jloerincs@lco.global>
2019-07-02 23:02:20 +0000

    Remove extra fluff

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,10 +1,6 @@
-import pytest
+import pytest, re
 from ptr_api.aws import s3
-from moto import mock_s3
-from flask import Flask
-import re
 
-@mock_s3
 def test_get_presigned_url():
     BUCKET_NAME = 'test_bucket'
     OBJECT_NAME = 'test_object'
@@ -17,16 +13,12 @@ def test_get_presigned_url():
     assert verify_url is not None
     assert verify_region is not None
 
-
-@mock_s3
 def test_get_presigned_post_url():
-   app = Flask(__name__)
-   with app.app_context():
-       BUCKET_NAME = 'test_bucket'
-       OBJECT_NAME = 'test_object'
+    BUCKET_NAME = 'test_bucket'
+    OBJECT_NAME = 'test_object'
 
-       response_dict = s3.get_presigned_post_url(BUCKET_NAME, OBJECT_NAME)
-       url = response_dict['url']
-       expected_url = "https://s3.amazonaws.com/" + BUCKET_NAME
+    response_dict = s3.get_presigned_post_url(BUCKET_NAME, OBJECT_NAME)
+    url = response_dict['url']
+    expected_url = "https://s3.amazonaws.com/" + BUCKET_NAME
 
-       assert url == expected_url
+    assert url == expected_url
